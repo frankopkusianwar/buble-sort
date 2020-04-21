7d1581296c8fde1031dbf01426eb292c9050cee7
frankopkusianwar <okirorfrank3@gmail.com>
2020-04-21 12:47:20 +0300

    add bubble_sort_by method

--- buble.rb
+++ buble.rb
@@ -12,5 +12,23 @@ def buble_sort(array)
     array
 end
 
-my_array = [1,3,5,2,7,9]
-puts buble_sort(my_array)
+def bubble_sort_by(array)
+    sorted = false
+    while sorted == false
+        sorted = true
+        array.each_with_index do |i, index|
+            if  index < array.size - 1 && yield(array[index],array[index+1]).positive?
+                array[index], array[index+1] = array[index+1], array[index]
+                sorted = false
+            end
+        end
+    end
+    array
+end
+
+my_array = [4,3,78,2,0,2]
+p buble_sort(my_array)
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
